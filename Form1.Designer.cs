4449af9b27e63d755ecdebca4284f94a42d134b0
Gustavo Sousa Diogo Silva <GAS823@student.bham.ac.uk>
2019-11-01 22:26:31 +0000

    Upload files

---
+++ Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace Zip_Move
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.labelInput = new System.Windows.Forms.Label();
+            this.labelOutput = new System.Windows.Forms.Label();
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.textBoxOutput = new System.Windows.Forms.TextBox();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // labelInput
+            // 
+            resources.ApplyResources(this.labelInput, "labelInput");
+            this.labelInput.Name = "labelInput";
+            // 
+            // labelOutput
+            // 
+            resources.ApplyResources(this.labelOutput, "labelOutput");
+            this.labelOutput.Name = "labelOutput";
+            // 
+            // textBoxInput
+            // 
+            resources.ApplyResources(this.textBoxInput, "textBoxInput");
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.ReadOnly = true;
+            this.textBoxInput.Click += new System.EventHandler(this.textBoxInput_Click);
+            // 
+            // textBoxOutput
+            // 
+            resources.ApplyResources(this.textBoxOutput, "textBoxOutput");
+            this.textBoxOutput.Name = "textBoxOutput";
+            this.textBoxOutput.ReadOnly = true;
+            this.textBoxOutput.Click += new System.EventHandler(this.textBoxOutput_Click);
+            // 
+            // buttonStart
+            // 
+            resources.ApplyResources(this.buttonStart, "buttonStart");
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.textBoxOutput);
+            this.Controls.Add(this.textBoxInput);
+            this.Controls.Add(this.labelOutput);
+            this.Controls.Add(this.labelInput);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelInput;
+        private System.Windows.Forms.Label labelOutput;
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.TextBox textBoxOutput;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+using System.IO.Compression;
+
+namespace Zip_Move
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBoxInput_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog.ShowDialog();
+            textBoxInput.Text = folderBrowserDialog.SelectedPath;
+            checkEmptyBox();
+        }
+
+        private void textBoxOutput_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog.ShowDialog();
+            textBoxOutput.Text = folderBrowserDialog.SelectedPath;
+            checkEmptyBox();
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            string pathFileName = textBoxInput.Text + ".zip";
+            ZipFile.CreateFromDirectory(textBoxInput.Text, pathFileName, CompressionLevel.Optimal, false);
+            File.Move(pathFileName, textBoxOutput.Text + pathFileName.Substring(textBoxInput.Text.LastIndexOf("\\")));
+        }
+
+        private void checkEmptyBox()
+        {
+            if (textBoxInput.Text != "Click here to select file/folder" && textBoxOutput.Text != "Click here to select folder") {
+                buttonStart.Enabled = true;
+            }
+        }
+    }
+}
